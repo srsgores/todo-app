5853ccff338fbcb40926c55ce9f4399d7b1789f7
Sean Goresht <seangores@gmail.com>
2019-08-09 18:06:41 -0600

    add ember-svg-jar update build config with svg icon paths

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,12 @@ const EmberApp = require("ember-cli/lib/broccoli/ember-app");
 
 module.exports = function(defaults) {
 	let app = new EmberApp(defaults, {
-		// Add options here
+		svgJar: {
+			sourceDirs: [
+				"public/assets/svg",
+				"public/assets/svg/icons"
+			]
+		}
 	});
 
 	/* Use `app.import` to add additional libraries to the generated output files.

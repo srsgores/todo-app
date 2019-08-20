8425c4ad6a0866d5ab80f51565cd00e0900553e4
Sean Goresht <seangores@gmail.com>
2019-08-20 11:45:10 -0600

    index route: use uppercase RSVP as per the convention

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,12 +1,12 @@
 import Route from "@ember/routing/route";
-import rsvp from "rsvp";
+import RSVP from "rsvp";
 
 export default Route.extend({
 	model() {
 		return this.store.findAll("todo");
 	},
 	setupController(controller, model) {
-		rsvp.hash({messages: this.store.findAll("message"), users: this.store.findAll("user")}).then((combinedPayload) => {
+		RSVP.hash({messages: this.store.findAll("message"), users: this.store.findAll("user")}).then((combinedPayload) => {
 			controller.setProperties(combinedPayload);
 		});
 		controller.set("today", new Date());

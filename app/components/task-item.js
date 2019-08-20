69aff45b88f08a3b6f277ed5cba9a87d5af85a25
Sean Goresht <seangores@gmail.com>
2019-08-20 14:43:32 -0600

    task-item: refactor component to allow toggling edit and complete

--- app/components/task-item.js
+++ app/components/task-item.js
@@ -1,3 +1,24 @@
 import Component from "@ember/component";
+import {computed} from "@ember/object";
 
-export default Component.extend({});
+export default Component.extend({
+	task: null,
+	classNameBindings: ["hasTitle", "hasDueDate", "hasDescription", "isEditing"],
+	isEditing: false,
+	hasTitle: computed.notEmpty("task.title"),
+	hasDueDate: computed.notEmpty("task.dueDate"),
+	hasDescription: computed.notEmpty("task.description"),
+	actions: {
+		onSave(task) {
+			return task.save().finally(() => {
+				this.toggleProperty("isEditing")
+			});
+		}
+	},
+	init() {
+		if (!this.get("hasTitle")) {
+			this.set("isEditing", true);
+		}
+		return this._super(...arguments);
+	}
+});

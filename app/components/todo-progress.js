3718087cf04d9278b2e4a513c52d034007cef756
Sean Goresht <seangores@gmail.com>
2019-08-20 11:24:36 -0600

    index: refactor todo-progress to have computed properties in component rather than in index controller remove index controller in favour of putting logic in components

--- app/components/todo-progress.js
+++ app/components/todo-progress.js
@@ -1,5 +1,13 @@
 import Component from "@ember/component";
+import {computed} from "@ember/object";
 
 export default Component.extend({
-	tagName: "aside"
+	tagName: "aside",
+	tasksCount: computed.alias("tasks.length"),
+	tasksRemaining: computed.filterBy("tasks", "complete", false),
+	tasksRemainingCount: computed.alias("tasksRemaining.length"),
+	tasksCompleted: computed.filterBy("tasks", "complete", true),
+	tasksCompletedCount: computed.alias("tasksCompleted.length"),
+	tasksToday: computed.gte("tasks", "today"),
+	incompleteTasksToday: computed.filterBy("tasksToday", "complete", false)
 });

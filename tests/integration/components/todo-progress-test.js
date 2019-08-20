bd809024569f36e2b8ed448db0d6d4b7ebaa8082
Sean Goresht <seangores@gmail.com>
2019-08-19 18:11:32 -0600

    components: add todo-progress component

---
+++ tests/integration/components/todo-progress-test.js
@@ -0,0 +1,26 @@
+import {render} from "@ember/test-helpers";
+import {setupRenderingTest} from "ember-qunit";
+import hbs from "htmlbars-inline-precompile";
+import {module, test} from "qunit";
+
+module("Integration | Component | todo-progress", function(hooks) {
+	setupRenderingTest(hooks);
+
+	test("it renders", async function(assert) {
+		// Set any properties with this.set('myProperty', 'value');
+		// Handle any actions with this.set('myAction', function(val) { ... });
+
+		await render(hbs`<TodoProgress />`);
+
+		assert.equal(this.element.textContent.trim(), "");
+
+		// Template block usage:
+		await render(hbs`
+      <TodoProgress>
+        template block text
+      </TodoProgress>
+    `);
+
+		assert.equal(this.element.textContent.trim(), "template block text");
+	});
+});

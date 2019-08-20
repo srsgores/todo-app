776adfe893831d2c186fc3598282c8fdd3d8f1c5
Sean Goresht <seangores@gmail.com>
2019-08-20 16:51:10 -0600

    mirage: load more than 1 default task

--- mirage/config.js
+++ mirage/config.js
@@ -2,7 +2,10 @@ export default function() {
 	this.get("/tasks", () => {
 		return {
 			data: [
-				{id: 1, type: "tasks", attributes: {title: "Make my bed", text: "Make my bed", complete: false, "created-at": new Date()}}
+				{id: 1, type: "tasks", attributes: {title: "Make my bed", text: "Lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum", complete: false, "created-at": new Date()}},
+				{id: 2, type: "tasks", attributes: {title: "Do the dishes", text: "Lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum", complete: false, "created-at": new Date()}},
+				{id: 3, type: "tasks", attributes: {title: "Walk the dog", text: "Lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum", complete: false, "created-at": new Date()}},
+				{id: 4, type: "tasks", attributes: {title: "Make lunch", text: "Lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum", complete: false, "created-at": new Date()}},
 			]
 		};
 	});

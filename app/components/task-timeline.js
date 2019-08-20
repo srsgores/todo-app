e1f3958feb0f35977165f0c2d0c1c56cc6a0986c
Sean Goresht <seangores@gmail.com>
2019-08-20 15:53:59 -0600

    task-timeline: allow filtering tasks by preset intervals using radio buttons

--- app/components/task-timeline.js
+++ app/components/task-timeline.js
@@ -1,8 +1,29 @@
 import Component from "@ember/component";
+import {computed} from "@ember/object";
 
 export default Component.extend({
 	tagName: "aside",
 	title: "Tasks",
-	filterName: "All Tasks",
-	tasks: null
+	filterName: "Last 7 Days",
+	tasks: null,
+	filteredTasks: computed("tasks", "startFilterDate", function() {
+		return this.get("tasks").filter((task) => task.get("createdAt") >= this.get("startFilterDate"));
+	}),
+	startFilterDate: null,
+	init() {
+		const dayIntervals = [-1, -14, -7, 7, 14];
+		this.set("startDates", dayIntervals.map((interval) => {
+			const today = new Date();
+			const otherDate = today.getDate() + interval;
+			today.setDate(otherDate);
+			return today;
+		}));
+		this.set("startFilterDate", this.get("startDates.firstObject"));
+		return this._super(...arguments);
+	},
+	actions: {
+		setFilterDate(day) {
+			return this.set("startFilterDate", day);
+		}
+	}
 });

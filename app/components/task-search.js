8e51c9777fe4b69ef7d8ba0882f326e4f46f42cf
Sean Goresht <seangores@gmail.com>
2019-08-20 22:39:34 -0600

    styles: finish styling header styles: add component stylesheet

--- app/components/task-search.js
+++ app/components/task-search.js
@@ -1,9 +1,12 @@
 import Component from "@ember/component";
 import {debounce} from "@ember/runloop";
+import {computed} from "@ember/object";
 
 export default Component.extend({
 	tagName: "form",
 	attributeBindings: ["role", "method", "action"],
+	classNameBindings: [":task-search", "hasValue"],
+	hasValue: computed.notEmpty("filter"),
 	role: "search",
 	method: "GET",
 	action: "/",

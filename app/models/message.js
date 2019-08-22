acb246a608e1c6d9fbb7da63713c521126e77ced
Sean Goresht <seangores@gmail.com>
2019-08-22 15:21:31 -0600

    message model: use title and text as aliases for subject and body

--- app/models/message.js
+++ app/models/message.js
@@ -1,9 +1,12 @@
 import DS from "ember-data";
 import Task from "./task";
+import {computed} from "@ember/object";
 
 export default Task.extend({
 	from: DS.belongsTo("user"),
 	to: DS.belongsTo("user"),
 	subject: DS.attr("string"),
+	title: computed.alias("subject"),
+	text: computed.alias("body"),
 	body: DS.attr("string")
 });

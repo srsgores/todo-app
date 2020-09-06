39a84090882ce7be93055cfe3954057bccd124fd
Sean Goresht <seangores@gmail.com>
2020-09-06 00:40:28 -0600

    refactor: update task-timeline component to octane

--- app/components/task-timeline.js
+++ app/components/task-timeline.js
@@ -1,32 +1,28 @@
 import {filter, sort} from "@ember/object/computed";
-import Component from "@ember/component";
+import Component from "@glimmer/component";
+import {tracked} from "@glimmer/tracking";
 
-export default Component.extend({
-	tagName: "aside",
-	title: "Tasks",
-	filterName: "Last 7 Days",
-	tasks: null,
-	filteredTasks: filter("tasks", ["startFilterDate"], function(task) {
-		return task.get("createdAt") >= this.startFilterDate;
-	}),
-	sortedFilteredTasks: sort("filteredTasks", "taskSorting"),
-	taskSorting: Object.freeze(["createdAt:desc"]),
-	startFilterDate: null,
-	classNameBindings: [":widget"],
-	init() {
-		const dayIntervals = [-1, 7, 14];
-		this.set("startDates", dayIntervals.map((interval) => {
+export default class TaskTimelineComponent extends Component {
+	title = "Tasks";
+	@tracked startFilterDate;
+	@filter("args.tasks", ["startFilterDate"], function(task) {
+		return task.get("createdAt") >= this.args.startFilterDate;
+	}) filteredTasks;
+	@sort("filteredTasks", "taskSorting") sortedFilteredTasks;
+
+	get taskSorting() {
+		return this.args.taskSorting || ["createdAt:desc"];
+	}
+
+	constructor() {
+		super(...arguments);
+		const dayIntervals = [-1, -14, -7, 7, 14];
+		this.startDates = dayIntervals.map((interval) => {
 			const today = new Date();
 			const otherDate = today.getDate() + interval;
 			today.setDate(otherDate);
 			return today;
-		}));
-		this.set("startFilterDate", this.get("startDates.firstObject"));
-		return this._super(...arguments);
-	},
-	actions: {
-		setFilterDate(day) {
-			return this.set("startFilterDate", day);
-		}
+		});
+		this.startFilterDate = this.startDates[0];
 	}
-});
+}

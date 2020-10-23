f929c3c4e6eec0982dc8264c0025d2a9a049cae0
Sean Goresht <seangores@gmail.com>
2020-10-22 22:01:01 -0600

    feat: enable couchdb adapter with config chore: remove mirage for now

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,14 +1,37 @@
-import PouchDB from "ember-pouch/pouchdb";
 import {Adapter} from "ember-pouch";
-
-let remote = new PouchDB("http://localhost:5984/todo");
-let db = new PouchDB("todo-app");
-
-db.sync(remote, {
-	live: true,   // do a live, ongoing sync
-	retry: true   // retry if the connection is lost
-});
+import PouchDB from "ember-pouch/pouchdb";
+import config from "todo-app/config/environment";
+import {isEmpty} from "@ember/utils";
+import {assert} from "@ember/debug";
 
 export default class ApplicationAdapter extends Adapter {
-	db = db;
+	constructor() {
+		super(...arguments);
+
+		const localDb = config.emberPouch.localDb;
+
+		assert("emberPouch.localDb must be set", !isEmpty(localDb));
+
+		const db = new PouchDB(localDb);
+		this.db = db;
+
+		// If we have specified a remote CouchDB instance, then replicate our local database to it
+		if (config.emberPouch.remoteDb) {
+			let remoteDb = new PouchDB(config.emberPouch.remoteDb);
+
+			db.sync(remoteDb, {
+				live: true,
+				retry: true
+			});
+		}
+
+		return this;
+	}
+
+	unloadedDocumentChanged(obj) {
+		let recordTypeName = this.getRecordTypeName(this.store.modelFor(obj.type));
+		this.db.rel.find(recordTypeName, obj.id).then((doc) => {
+			this.store.pushPayload(recordTypeName, doc);
+		});
+	}
 }
--- config/environment.js
+++ config/environment.js
@@ -6,6 +6,10 @@ module.exports = function(environment) {
 		environment,
 		rootURL: "/",
 		locationType: "auto",
+		emberPouch: {
+			remoteDb: "http://localhost:5984/todo",
+			localDb: "todo"
+		},
 		EmberENV: {
 			FEATURES: {
 				// Here you can enable experimental features on an ember canary build

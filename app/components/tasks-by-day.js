c2b5ec56acb5c58ace2bd7fa711988eca1cebd3c
Sean Goresht <seangores@gmail.com>
2020-10-26 02:09:01 -0600

    feat: allow grouping tasks by dueDate

--- app/components/tasks-by-day.js
+++ app/components/tasks-by-day.js
@@ -1,4 +1,18 @@
 import Component from "@glimmer/component";
+import {computed} from "@ember/object";
 
 export default class TasksByDayComponent extends Component {
+	@computed.filterBy("args.tasks", "dueDate", Boolean) tasksWithDueDate;
+	get tasksByDueDate() {
+		let groupedTasksByDate = {};
+		if (this.tasksWithDueDate) {
+			this.args.tasks.forEach((task) => {
+				const day = task.dueDate?.getDay();
+				if (day) {
+					groupedTasksByDate[day] = groupedTasksByDate[day] ? [...groupedTasksByDate[day], task] : [task];
+				}
+			});
+		}
+		return groupedTasksByDate;
+	}
 }

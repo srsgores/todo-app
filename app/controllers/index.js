8c3d0ab757cacc12597e70df13ca2b66e6bd5107
Sean Goresht <seangores@gmail.com>
2020-10-26 01:23:31 -0600

    feat: wip: add tasks by day view and query param

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,7 +1,9 @@
 import Controller from "@ember/controller";
-import {action} from "@ember/object";
+import {action, computed} from "@ember/object";
 
 export default class IndexController extends Controller {
+	queryParams = ["viewMode"];
+	@computed.equal("viewMode", "calendar") isCalendarMode;
 	@action
 	async addTask() {
 		const task = this.store.createRecord("task", {createdAt: new Date()});
